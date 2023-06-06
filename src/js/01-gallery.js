60b3d0db11326896ef8471f077103da7b684ba55
Andew1234 <andriidziapka101@gmail.com>
2023-06-06 23:51:02 +0300

    hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,55 +3,29 @@ import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
 const galleryList = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 
-galleryList.addEventListener('click', onGalleryListClick);
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 function createGalleryMarkup(pictures) {
   return pictures
     .map(({ original, preview, description }) => {
       return `<li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </li>`;
+      <a class="gallery__link" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+   </li>`;
     })
     .join('');
-}
-
-function onGalleryListClick(evt) {
-  evt.preventDefault();
-
-  const galleryItem = evt.target.closest('.gallery__item');
-  if (!galleryItem) {
-    return;
-  }
-
-  const image = galleryItem.querySelector('.gallery__image');
-  const largeImageUrl = image.dataset.source;
-
-  const pressEsc = event => {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  };
-
-  const instance = basicLightbox.create(`<img src="${largeImageUrl}">`, {
-    onShow: instance => {
-      document.addEventListener('keydown', pressEsc);
-    },
-    onClose: instance => {
-      document.removeEventListener('keydown', pressEsc);
-    },
-  });
-
-  instance.show();
 }
